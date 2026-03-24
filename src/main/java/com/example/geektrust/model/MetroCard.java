1323515b0d62c9f0965c5b18a679700c6de5a6de
Rohit Kapare <rakapare86@gmail.com>
2026-03-24 23:30:29 +0530

    Refactored MetroCard class

--- src/main/java/com/example/geektrust/model/MetroCard.java
+++ src/main/java/com/example/geektrust/model/MetroCard.java
@@ -14,10 +14,6 @@ public class MetroCard {
     return balance;
   }
 
-  public void setBalance(int balance) {
-    this.balance = balance;
-  }
-
   public String getCardNumber() {
     return cardNumber;
   }
